3966331facc7f4cfc3974b669e512b5dc412ae22
sharavanacoder <138202301+sharavanacoder@users.noreply.github.com>
2023-07-27 21:31:53 +0530

    Update notes.js

--- controller/notes.js
+++ controller/notes.js
@@ -1,44 +1,50 @@
-const Notes=require('../models/notes');
+const Notes = require('../models/notes');
 
-module.exports.renderNewNote=(req, res) => {
+module.exports.renderNewNote = (req, res) => {
     res.render('notes/new');
 }
-module.exports.createNote=async (req, res) => {
-    const { title, content }=req.body;
-    const note=new Notes({ title, content });
-    note.user=req.user._id;
+module.exports.createNote = async (req, res) => {
+    const { title, content } = req.body;
+    const note = new Notes({ title, content });
+    note.user = req.user._id;
+    let now = new Date();
+    note.offsetCreated = note.date.getTimezoneOffset()
     await note.save({ timestamps: { createdAt: true, updatedAt: false } });
     req.flash('success', 'Successfully created a new note!');
     res.redirect('/show');
 }
 
-module.exports.renderShowNotes=async (req, res) => {
-    const notes=await Notes.find({ user: req.user._id });
+module.exports.renderShowNotes = async (req, res) => {
+    const notes = await Notes.find({ user: req.user._id });
     res.render('notes/show', { notes });
 }
 
-module.exports.renderEditNote=async (req, res) => {
+module.exports.renderEditNote = async (req, res) => {
     try {
-        const { id }=req.params;
-        const note=await Notes.findById(id);
+        const { id } = req.params;
+        const note = await Notes.findById(id);
         res.render('notes/edit', { note });
     } catch (e) { res.render('error'); }
-}   
-module.exports.editNote=async (req, res) => {
+}
+module.exports.editNote = async (req, res) => {
     try {
-        const { title, content }=req.body;
-        const note=await Notes.findById(req.params.id);
-        note.title=title;
-        note.content=content;
+        const { title, content } = req.body;
+        const note = await Notes.findById(req.params.id);
+        note.title = title;
+        note.content = content;
+        let now = new Date();
+        note.offsetUpdated = now.getTimezoneOffset();
+        note.updated = new Date(now);
         await note.save({ timestamps: { createdAt: false, updatedAt: true } });
         req.flash('success', 'Successfully Note is Updated!');
         res.redirect('/show');
+
     }
     catch (e) {
         return res.redirect('/error');
     }
 }
-module.exports.deleteNote=async (req, res) => {
+module.exports.deleteNote = async (req, res) => {
     try {
         await Notes.findByIdAndDelete(req.params.id);
         req.flash('success', 'Successfully Note is Deleted!!!');
