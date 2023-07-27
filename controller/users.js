9579f5aab403e588db16233057cfdc8ff4e7586b
sharavanacoder <138202301+sharavanacoder@users.noreply.github.com>
2023-07-27 09:26:48 +0530

    Update users.js

--- controller/users.js
+++ controller/users.js
@@ -1,61 +1,53 @@
-const User=require('../models/users');
-const passport=require('passport');
+const User = require('../models/users');
+const passport = require('passport');
 const emailExistence = require("@stationf/email-existence");
 
 
-module.exports.renderRegister=(req, res, next) => {
-    res.render('user/register');   
+module.exports.renderRegister = (req, res, next) => {
+  res.render('user/register');
 }
-module.exports.register=async (req, res, next) => {
-    const { email, password, username }=req.body;
-    let check;
-    const isPresent=await User.findOne({ email });
-    if (isPresent) {
-      req.flash("error", 'Email already registered!!!');
-      return res.redirect('/signup');
-  }
-  if (!email||!password||!username)
-  {
+module.exports.register = async (req, res, next) => {
+  const { email, password, username } = req.body;
+  let check;
+  if (!email || !password || !username) {
     req.flash("error", 'Please enter the fields!');
     return res.redirect('/signup');
   }
   try {
-    check=await emailExistence.check(email);
+    check = await emailExistence.check(email);
     const user = new User({ email, username });
     const registerUser = await User.register(user, password);
     console.log(registerUser);
     req.login(registerUser, err => {
       if (err) {
-        req.flash('error', 'Email is invalid!!!');
+        req.flash('error', 'Email is already registered');
         return res.redirect('/signup');
       }
       req.flash('success', 'Welcome to Notes App!');
-        return res.redirect('/show');    
-        })
+      return res.redirect('/show');
+    })
   } catch (e) {
-      req.flash('error', 'Email is invalid!!!');
-      return res.redirect('/signup');
+    console.log(e)
+    req.flash('error', 'Invalid Email!!! or already registered');
+    return res.redirect('/signup');
   }
 }
 
-module.exports.renderLogin=(req, res) => {
-    console.log('login', req.isAuthenticated());
-    if(!req.isAuthenticated());
-        return res.render('user/signin');
-    return res.render('/show')
-   
+module.exports.renderLogin = (req, res) => {
+  console.log('login', req.isAuthenticated());
+  return res.render('user/signin');
 }
-module.exports.login=async (req, res) => {
-    req.flash("success", 'Welcome Back!!!');
-    res.redirect('/show');
+module.exports.login = async (req, res) => {
+  req.flash("success", 'Welcome Back!!!');
+  res.redirect('/show');
 };
-module.exports.logout=(req, res) => {
-    req.logout(function(err) {
-        if (err) { 
-            return res.render('error');
-        }
+module.exports.logout = (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return res.render('error');
+    }
     req.flash('success', "Goodbye!");
     res.redirect('/');
   });
-   
+
 }
